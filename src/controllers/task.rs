ab63cea87563668ea7302ba030cf25e7611449c7
mojila <me.mojila@gmail.com>
2022-08-24 21:58:01 +0700

    day4 - change to mod based and add connection to sqlite

--- src/controllers/task.rs
+++ src/controllers/task.rs
@@ -2,9 +2,11 @@ use rocket::serde::{json::Json};
 use std::collections::HashMap;
 use uuid::Uuid;
 use crate::dtos::{GetAllDto, Task};
+use crate::databases::Tasks;
+use rocket_db_pools::Connection;
 
 #[get("/")]
-pub fn get_all() -> Json<GetAllDto> {
+pub async fn get_all(mut _db: Connection<Tasks>) -> Json<GetAllDto> {
     let mut tasks = HashMap::new();
     let id = Uuid::new_v4().to_string();
     
